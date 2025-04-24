924ca1a3d9fdf19424f55b0f5b38f99b45c0d504
slowlylin <babelin@gmail.com>
2025-04-24 09:34:57 +0800

    trigger redeploy

--- api/chat.js
+++ api/chat.js
@@ -17,3 +17,5 @@ export default async function handler(req, res) {
   const data = await response.json();
   res.status(200).json({ reply: data.choices[0].message.content });
 }
+
+// redeploy trigger
